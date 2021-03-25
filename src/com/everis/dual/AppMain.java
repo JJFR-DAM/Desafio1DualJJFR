2f577bab0673df7edd85ccfa9ee53f04fd57cc27
JuFerNar <fernandez.rojua20@salesianos.cadiz.edu>
2021-03-25 14:19:54 +0100

    Desafio V.1

---
+++ src/com/everis/dual/AppMain.java
@@ -0,0 +1,11 @@
+package com.everis.dual;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo");
+	}
+
+}
